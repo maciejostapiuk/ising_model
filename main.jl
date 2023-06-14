abcc96be6d9b72d434fd257e346671f10bfad2a0
Maciej Ostapiuk <maciejostapiuk@Maciejs-MacBook-Pro.local>
2023-06-15 00:51:46 +0200

    .

--- main.jl
+++ main.jl
@@ -252,6 +252,18 @@ ylabel!("E");
 savefig("trajectories_T=2,27_L=80");
 ###
 
+### Trajectories for T == 2,27, p = 80
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(3,50000,80)
+    plot!(1:50000, res, legend = false)
+end
+title!("trajectories for T = 3");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=3_L=80");
+###
 
 ### Trajectories for T == 4.8, p = 10
 #
@@ -302,18 +314,25 @@ xlabel!("time[MCS]");
 ylabel!("E");
 savefig("trajectories_T=4,8_L=80");
 ###
-rnge = 0.5:0.01:3.5
+rnge = 0.5:0.1:3.5
 ### plot Magnetisation of T
 plt = plot(); 
-results_10 = [1/100*sum(1/10*sum([ising_model(elem, 100,10) for _ in 1:10])) for elem in rnge]
+results_10 = [1/100*sum(1/10*abs(sum([ising_model(elem, 100,10) for _ in 1:10]))) for elem in rnge]
 results_20 = [1/100*sum(1/10*sum([ising_model(elem, 100,20) for _ in 1:10])) for elem in rnge]
 results_40 = [1/100*sum(1/10*sum([ising_model(elem, 100,40) for _ in 1:10])) for elem in rnge]
 results_80 = [1/100*sum(1/10*sum([ising_model(elem, 100,80) for _ in 1:10])) for elem in rnge]
 plot!(rnge, results_10, label = "L = 10")
-plot!(rnge, results_20, label = "L = 20")
+scatter!(rnge, abs.(results_20), label = "L = 20")
 plot!(rnge, results_40, label = "L = 40")
 plot!(rnge, results_80, label = "L = 80")
 xlabel!("T")
 ylabel!("m");
 title!("m(T)");
 savefig("megnetization of temperature");
+heatmap(rand([-1,1], 10,10))
+title!("0 MCS")
+savefig("0_mcs_10")
+
+heatmap(rand([-1,1], 80,80))
+title!("0 MCS")
+savefig("0_mcs_80")     
