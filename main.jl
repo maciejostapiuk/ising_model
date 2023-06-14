e0d81cac16337363d9c3db2fc1f8f3497d8b9013
Maciej Ostapiuk <maciejostapiuk@Maciejs-MacBook-Pro.local>
2023-06-14 14:14:16 +0200

    added plotting

--- main.jl
+++ main.jl
@@ -1,34 +1,308 @@
-using Plots;
+using Plots
+using DelimitedFiles
 ###
 Plots.default(show = true)
-#J = 1 
-#T = 1.85
-#N = 1000
-#p = 100
-#an = Plots.Animation()
-
 function ising_model(T, N, p)
-#plt = plot()
-for _ in 1:10
-    matrix = rand([-1,1], p,p)
-    mean_spin_value = zeros(N)
-    for mcs in 1:N
-        for iter in 1:p^2
-            i = rand(1:p)
-            j = rand(1:p)
-            energy = 2*matrix[i,j]*(matrix[mod1(i-1, p),j] + matrix[mod1(i+1,p), j] + matrix[i,mod1(j-1,p)] + matrix[i, mod1(j+1,p)])
-            if energy <= 0
-                matrix[i,j] *= -1
-            else
-                x = rand()
-                if x < exp(-energy/T)
-                    matrix[i,j] *=-1
+        matrix = rand([-1,1], p,p)
+        mean_spin_value = zeros(N)
+        for mcs in 1:N
+            for iter in 1:p^2
+                i = rand(1:p)
+                j = rand(1:p)
+                energy = 2*matrix[i,j]*(matrix[mod1(i-1, p),j] + matrix[mod1(i+1,p), j] + matrix[i,mod1(j-1,p)] + matrix[i, mod1(j+1,p)])
+                if energy <= 0
+                    matrix[i,j] *= -1
+                else
+                    x = rand()
+                    if x < exp(-energy/T)
+                        matrix[i,j] *=-1
+                    end
                 end
-            end
             mean_spin_value[mcs] = sum(matrix)/p^2
+            end
+        end
+        #writedlm("spins_configuration.txt", matrix)
+        #writedlm("trajectories.txt", mean_spin_value)
+        return mean_spin_value
     end
-    plot!(1:N, mean_spin_value,legend = false)
-    end
-    end
+
+### Trajectories for T == 0, p = 100 
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(0,5000,100)
+    plot!(1:5000, res, legend = false)
 end
-ising_model(1.85, 3000, 10)
+title!("trajectories for T = 0")
+xlabel!("time[MCS]")
+ylabel!("E")
+display(plt)
+savefig("trajectories_T=0_L=100")
+###
+### Trajectories for T == 1, p = 10
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(1,5000,10)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 1");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=1_L=10");
+###
+
+### Trajectories for T == 1, p = 20
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(1,5000,20)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 1");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=1_L=20");
+###
+### Trajectories for T == 1, p = 40
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(1,5000,40)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 1");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=1_L=40");
+###
+### Trajectories for T == 1, p = 80
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(1,5000,80)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 1");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=1_L=80");
+###
+Plots.default(show = true)
+
+function ising_model_current_configuration(T, N, p)
+        matrix = rand([-1,1], p,p)
+        mean_spin_value = zeros(N)
+        for mcs in 1:N
+            for iter in 1:p^2
+                i = rand(1:p)
+                j = rand(1:p)
+                energy = 2*matrix[i,j]*(matrix[mod1(i-1, p),j] + matrix[mod1(i+1,p), j] + matrix[i,mod1(j-1,p)] + matrix[i, mod1(j+1,p)])
+                if energy <= 0
+                    matrix[i,j] *= -1
+                else
+                    x = rand()
+                    if x < exp(-energy/T)
+                        matrix[i,j] *=-1
+                    end
+                end
+            mean_spin_value[mcs] = sum(matrix)/p^2
+            end
+        end
+        #writedlm("spins_configuration.txt", matrix)
+        #writedlm("trajectories.txt", mean_spin_value)
+        return matrix
+    end
+
+
+### current config for T = 1
+### a) L = 10 
+res = ising_model_current_configuration(1, 100, 10)
+heatmap(res);
+title!("100 MCS, T = 1, sieć 10 x 10");
+savefig(("100 MCS, T = 1, sieć 10 x 10"));
+### b) L = 80
+res = ising_model_current_configuration(1, 100, 80)
+heatmap(res);
+title!("100 MCS, T = 1, sieć 80 x 80");
+savefig(("100 MCS, T = 1, sieć 80 x 80"));
+###
+
+### current config for T = 2.26
+### a) L = 10
+res = ising_model_current_configuration(2.26, 100, 10)
+heatmap(res);
+title!("100 MCS, T = 2.26, sieć 10 x 10");
+savefig(("100 MCS, T = 2,26, sieć 10 x 10"));
+###b) L = 80
+res = ising_model_current_configuration(2.26, 100, 80)
+heatmap(res);
+title!("100 MCS, T = 2.26, sieć 80 x 80");
+savefig(("100 MCS, T = 2,26, sieć 80 x 80"));
+
+### current config for T = 4
+### a) L = 10
+res = ising_model_current_configuration(4, 100, 10)
+heatmap(res);
+title!("100 MCS, T = 4, sieć 10 x 10");
+savefig(("100 MCS, T =4, sieć 10 x 10"));
+###b) L = 80
+res = ising_model_current_configuration(4, 100, 80)
+heatmap(res);
+title!("100 MCS, T = 4, sieć 80 x 80");
+
+### Trajectories for T == 1.85, p = 10
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(1.85,5000,10)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T= 1,85");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=1,85_L=10");
+###
+
+### Trajectories for T == 1.85, p = 20
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(1.85,5000,20)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 1,85");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=1,85_L=20");
+###
+### Trajectories for T == 1.85, p = 40
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(1.85,5000,40)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 1,85");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=1,85_L=40");
+###
+### Trajectories for T == 1.85, p = 80
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(1.85,5000,80)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 1,85");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=1,85_L=80");
+###
+
+
+### Trajectories for T == 2.27, p = 10
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(2.27,5000,10)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T=2,27 = 1");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=2,27_L=10");
+###
+
+### Trajectories for T == 2.27, p = 20
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(2.27,5000,20)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 2,27");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=2,27_L=20");
+###
+### Trajectories for T == 2,27, p = 40
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(2.27,5000,40)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 2,27");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=2,27_L=40");
+###
+### Trajectories for T == 2,27, p = 80
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(2.27,5000,80)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 2,27");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=2,27_L=80");
+###
+
+
+### Trajectories for T == 4.8, p = 10
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(4.8,5000,10)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T=4,8");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=4,8_L=10");
+###
+
+### Trajectories for T == 4.8, p = 20
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(4.8,5000,20)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 4,8");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=4,8_L=20");
+###
+### Trajectories for T == 4,8, p = 40
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(4.8,5000,40)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 4,8");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=4,8_L=40");
+###
+### Trajectories for T == 4,8, p = 80
+#
+plt = plot();
+for i in 1:10
+    res = ising_model(4.8,5000,80)
+    plot!(1:5000, res, legend = false)
+end
+title!("trajectories for T = 4,8");
+xlabel!("time[MCS]");
+ylabel!("E");
+savefig("trajectories_T=4,8_L=80");
+###
+
+### plot Magnetisation of T
+
+
